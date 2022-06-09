993bbf578bbcaacb95ec8bea247e44bbd91e77d7
Ashikur Rahman <smashikurrahman24@gmail.com>
2022-06-09 21:11:29 +0600

    largest element

--- largest.js
+++ largest.js
@@ -1,5 +1,13 @@
 function largestNumber(ages){
-    for(let i = 0; i < ages.length; i ++){
-        
+    let element = 12;
+    for(let i = 0; i < ages.length; i++){
+        let age = ages[i]
+        if(age < element){
+            element=age
+        }
     }
+    return element
 }
+const ages=[12, -7, 3, 16, -32, 55, 43]
+let largestAge = largestNumber(ages)
+console.log(largestAge);
